422901f024e9c38eeb99e152d1ffcf7a85bad9c9
Chris <cohabo@gmail.com>
2013-04-23 23:42:01 +0300

    project files loaded to vs && first commit

---
+++ Visual Micro/.traffic_logger.vsarduino.h
@@ -0,0 +1,37 @@
+//Board = Arduino Mega 2560 or Mega ADK
+#define ARDUINO 104
+#define __AVR_ATmega2560__
+#define F_CPU 16000000L
+#define __AVR__
+#define __cplusplus
+#define __attribute__(x)
+#define __inline__
+#define __asm__(x)
+#define __extension__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
+#define __inline__
+#define __asm__ 
+#define __volatile__
+#define __builtin_va_list
+#define __builtin_va_start
+#define __builtin_va_end
+#define __DOXYGEN__
+#define prog_void
+#define PGM_VOID_P int
+#define NOINLINE __attribute__((noinline))
+
+typedef unsigned char byte;
+extern "C" void __cxa_pure_virtual() {}
+
+//already defined in arduno.h
+//already defined in arduno.h
+void print_memory();
+void raw_print_memory();
+void erase_memory();
+void make_tone();
+
+#include "C:\Program Files (x86)\arduino-1.0.4\hardware\arduino\variants\mega\pins_arduino.h" 
+#include "C:\Program Files (x86)\arduino-1.0.4\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\Users\cohabo\dev\Arduino\traffic_logger\traffic_logger.pde"
+#include "C:\Users\cohabo\dev\Arduino\traffic_logger\EEPROMAnything.h"
